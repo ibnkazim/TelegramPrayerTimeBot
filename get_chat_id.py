d0e39feffbbb3cce6014b3e6bb2ba91edb53664a
EmilEbulisov <qirimtatarbala@gmail.com>
2025-05-24 20:13:19 +0300

    chat id script update

--- get_chat_id.py
+++ get_chat_id.py
@@ -1,2 +1,10 @@
 from telegram import Bot
 import asyncio
+
+async def get_chat_id():
+    bot = Bot(token="7402773153:AAFxzD6OGtPEdsH87l9v_sfv6_3nOlg3PD4")
+    updates = await bot.get_updates()
+    for update in updates:
+        print(f"Chat ID: {update.message.chat.id}")
+
+asyncio.run(get_chat_id())
