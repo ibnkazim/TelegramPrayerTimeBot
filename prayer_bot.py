7dd6e4e0034d464a24805852e39989d5d7214064
EmilEbulisov <qirimtatarbala@gmail.com>
2025-05-24 20:25:20 +0300

    change code

--- prayer_bot.py
+++ prayer_bot.py
@@ -12,3 +12,23 @@ import os
 BOT_TOKEN = "7402773153:AAFxzD6OGtPEdsH87l9v_sfv6_3nOlg3PD4"  
 PRAYER_URL = "https://qmdi.ru/raspisanie-namazov/"
 SUBSCRIBERS_FILE = "subscribers.json"
+
+bot = telegram.Bot(token=BOT_TOKEN)
+updater = Updater(BOT_TOKEN, use_context=True)
+
+# Хранилище расписания намаза и подписчиков
+prayer_times = {}
+subscribers = set()
+
+def load_subscribers():
+    """Загрузка подписчиков из файла"""
+    global subscribers
+    if os.path.exists(SUBSCRIBERS_FILE):
+        with open(SUBSCRIBERS_FILE, "r") as f:
+            subscribers = set(json.load(f))
+    return subscribers
+
+def save_subscribers():
+    """Сохранение подписчиков в файл"""
+    with open(SUBSCRIBERS_FILE, "w") as f:
+        json.dump(list(subscribers), f)
